45380e04f8c3fe13c278747a6730edb8eab36482
rkarthik-iamok <rkarthik.iamok@gmail.com>
2022-12-31 10:20:22 -0500

    Added Index page and some styling

--- coffeebrain/app.js
+++ coffeebrain/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 // Import Index Router
 const indexRouter = require('./routes/indexRouter.js');
 
 // Initialize the express app
 const app = express();
+
+// Set the static files
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// Set the views
+app.set('views', 'views');
+app.set('view engine', 'ejs');
 
 // Add Routes
 app.use('/', indexRouter);
